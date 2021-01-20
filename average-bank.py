0670b5e059bf26d774a29df43779ea49db9299d5
Josh Giersch <josh@josh.sg>
2021-01-19 17:42:06 -0800

    cleaning up and fixing reverse

--- average-bank.py
+++ average-bank.py
@@ -3,14 +3,13 @@ import string
 
 def GetJSON(url):
     try:
-        response = requests.get(url)
-        response.raise_for_status()
+        r = requests.get(url)
+        r.raise_for_status()
     except:
-        print(url + " returned error code " + str(response.status_code))
+        print(url + " returned error code " + str(r.status_code))
         raise
     else:
-        response_data = response.json()
-        return response_data
+        return r.json()
 
 words = {}
 FDICFile = "https://banks.data.fdic.gov/api/institutions?filters=ACTIVE%3A1&fields=NAME&sort_by=OFFICES&sort_order=DESC&limit=10000&offset=0&format=json&download=false&filename=data_file"
@@ -25,6 +24,6 @@ for bank in dataBlob:
         else:
             words[s] = 1
                 
-for w in sorted(words, key=words.get, reverse=True):
+for w in sorted(words, key=words.get, reverse=False):
     print(w, words[w])
 
